3b515887669298a26e6ad3146f8e89fe3228e781
Alvaro Piorno <alvaropiorno100@gmail.com>
2021-09-16 00:07:31 -0300

    initial spring boot project

---
+++ src/main/kotlin/com/arqunq/mercadillolibre/MercadillolibreApplication.kt
@@ -0,0 +1,11 @@
+package com.arqunq.mercadillolibre
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MercadillolibreApplication
+
+fun main(args: Array<String>) {
+	runApplication<MercadillolibreApplication>(*args)
+}
---
+++ src/test/kotlin/com/arqunq/mercadillolibre/MercadillolibreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.arqunq.mercadillolibre
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MercadillolibreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
